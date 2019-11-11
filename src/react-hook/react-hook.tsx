8d5cc2faa9bcd7c303c1e0ea88725adb47327c00
Brandon Istenes <bistenes@gmail.com>
2019-11-11 09:04:09 -0600

    Remove cruft; fix undefined check

--- src/react-hook/react-hook.tsx
+++ src/react-hook/react-hook.tsx
@@ -13,17 +13,15 @@ export const ModuleNameContext = React.createContext(null);
 let config;
 export function useConfig() {
   const moduleName = React.useContext(ModuleNameContext);
-  //  const [config, setConfig]: [null | Object, Function] = React.useState(null);
   if (!moduleName) {
     throw Error(
       "ModuleNameContext has not been provided. This should come from openmrs-react-root-decorator"
     );
   }
-  if (typeof config === "undefined") {
+  if (config == null) {
     // React will prevent the client component from rendering until the promise resolves
     throw Config.getConfig(moduleName).then(res => {
       config = res;
-      return config;
     });
   } else {
     return config;
